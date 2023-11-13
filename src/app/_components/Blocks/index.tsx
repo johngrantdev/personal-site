9bae011ca059fbcb720fbec73b3b6f5d53fad6d9
John Grant <john@raur.net>
2023-11-13 14:26:08 +1100

    removed comment block, update post collection name

--- src/app/_components/Blocks/index.tsx
+++ src/app/_components/Blocks/index.tsx
@@ -3,10 +3,9 @@ import React, { Fragment } from 'react'
 import { Page } from '../../../payload/payload-types.js'
 import { ArchiveBlock } from '../../_blocks/ArchiveBlock'
 import { CallToActionBlock } from '../../_blocks/CallToAction'
-import { CommentsBlock, type CommentsBlockProps } from '../../_blocks/Comments/index'
 import { ContentBlock } from '../../_blocks/Content'
 import { MediaBlock } from '../../_blocks/MediaBlock'
-import { RelatedPosts, type RelatedPostsProps } from '../../_blocks/RelatedPosts'
+import { RelatedArtifacts, type RelatedArtifactsProps } from '../../_blocks/RelatedArtifacts'
 import { toKebabCase } from '../../_utilities/toKebabCase'
 import { BackgroundColor } from '../BackgroundColor'
 import { VerticalPadding, VerticalPaddingOptions } from '../VerticalPadding'
@@ -16,12 +15,11 @@ const blockComponents = {
   content: ContentBlock,
   mediaBlock: MediaBlock,
   archive: ArchiveBlock,
-  relatedPosts: RelatedPosts,
-  comments: CommentsBlock,
+  relatedArtifacts: RelatedArtifacts,
 }
 
 export const Blocks: React.FC<{
-  blocks: (Page['layout'][0] | RelatedPostsProps | CommentsBlockProps)[]
+  blocks: (Page['layout'][0] | RelatedArtifactsProps)[]
   disableTopPadding?: boolean
 }> = props => {
   const { disableTopPadding, blocks } = props
