6bf5f547d1f20e111702db546aa645dd0402df0d
John Grant <john@raur.net>
2023-11-17 00:17:24 +1100

    update seed data to use lexical richtext

--- src/payload/seed/home.ts
+++ src/payload/seed/home.ts
@@ -11,32 +11,90 @@ export const home: Partial<Page> = {
   },
   hero: {
     type: 'lowImpact',
-    richText: [
-      {
+    richText: {
+      root: {
+        type: 'root',
+        format: '',
+        indent: 0,
+        version: 1,
+        direction: 'ltr',
         children: [
           {
-            text: 'Digital and Physical Product Development.',
+            type: 'heading',
+            tag: 'h1',
+            format: '',
+            indent: 0,
+            version: 1,
+            direction: 'ltr',
+            children: [
+              {
+                mode: 'normal',
+                type: 'text',
+                style: '',
+                detail: 0,
+                version: 1,
+                format: 1,
+                text: 'Digital and Physical Product Development.',
+              },
+            ],
+          },
+          {
+            type: 'paragraph',
+            format: '',
+            indent: 0,
+            version: 1,
+            direction: 'ltr',
+            children: [
+              {
+                mode: 'normal',
+                type: 'text',
+                style: '',
+                detail: 0,
+                version: 1,
+                format: 1,
+                text: 'Subheading description.',
+              },
+            ],
           },
         ],
-        type: 'h1',
       },
-    ],
+    },
     media: '{{IMAGE_1}}',
   },
   layout: [
     {
       blockName: 'Archive Block',
       blockType: 'archive',
-      introContent: [
-        {
-          type: 'h4',
+      introContent: {
+        root: {
+          type: 'root',
+          format: '',
+          indent: 0,
+          version: 1,
+          direction: 'ltr',
           children: [
             {
-              text: '',
+              type: 'heading',
+              tag: 'h4',
+              format: '',
+              indent: 0,
+              version: 1,
+              direction: 'ltr',
+              children: [
+                {
+                  mode: 'normal',
+                  type: 'text',
+                  style: '',
+                  detail: 0,
+                  version: 1,
+                  format: 1,
+                  text: '',
+                },
+              ],
             },
           ],
         },
-      ],
+      },
       populateBy: 'collection',
       relationTo: 'artifacts',
       limit: 12,
@@ -44,383 +102,37 @@ export const home: Partial<Page> = {
       categories: [],
     },
     {
-      blockName: 'Content Block',
-      blockType: 'content',
-      columns: [
-        {
-          size: 'full',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Core features',
-                },
-              ],
-              type: 'h2',
-            },
-            {
-              children: [
-                {
-                  text: '',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Admin Dashboard',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: "Manage this site's pages, artifacts and more from the ",
-                },
-                {
-                  type: 'link',
-                  linkType: 'custom',
-                  url: '/admin',
-                  children: [
-                    {
-                      text: 'admin dashboard',
-                    },
-                  ],
-                },
-                {
-                  text: '.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Authentication',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Complete user ',
-                },
-                {
-                  type: 'link',
-                  linkType: 'custom',
-                  url: '/login',
-                  children: [
-                    {
-                      text: 'login',
-                    },
-                  ],
-                },
-                {
-                  text: ' and ',
-                },
-                {
-                  type: 'link',
-                  linkType: 'custom',
-                  url: '/create-account',
-                  children: [
-                    {
-                      text: 'create account',
-                    },
-                  ],
-                },
-                {
-                  text: ' flows with email verification and password reset.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Preview',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Using versions, drafts, and preview, editors can review and share their changes before publishing them.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Comments',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Users can comment on artifacts and editors can moderate comments directly from the ',
-                },
-                {
-                  type: 'link',
-                  linkType: 'custom',
-                  url: '/admin/collections/comments',
-                  children: [
-                    {
-                      text: 'admin dashboard',
-                    },
-                  ],
-                },
-                {
-                  text: '.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'User Accounts',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Users can ',
-                },
-                {
-                  type: 'link',
-                  linkType: 'custom',
-                  url: '/account',
-                  children: [
-                    {
-                      text: 'manage their account',
-                    },
-                  ],
-                },
-                {
-                  text: ', view their comment history, and more without leaving the site.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Premium Content',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Easily restrict access to premium content to only authenticated members of your site.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Page Builder',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Custom page builder allows you to create unique page and artifact layouts for any type of content.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'SEO',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Editors have complete control over SEO data and site content directly from the ',
-                },
-                {
-                  type: 'link',
-                  linkType: 'custom',
-                  url: '/admin',
-                  children: [
-                    {
-                      text: 'admin dashboard',
-                    },
-                  ],
-                },
-                {
-                  text: '.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-        {
-          size: 'oneThird',
-          richText: [
-            {
-              children: [
-                {
-                  text: 'Dark Mode',
-                },
-              ],
-              type: 'h3',
-            },
-            {
-              children: [
-                {
-                  text: 'Users will experience this site in their preferred color scheme and each block can be inverted.',
-                },
-              ],
-            },
-          ],
-          enableLink: false,
-          link: {
-            reference: null,
-            url: '',
-            label: '',
-          },
-        },
-      ],
-    },
-    {
-      blockType: 'mediaBlock',
-      blockName: 'Media Block',
-      position: 'default',
-      media: '{{IMAGE_2}}',
-    },
-    {
       blockType: 'cta',
       blockName: 'CTA',
-      richText: [
-        {
+      richText: {
+        root: {
+          type: 'root',
+          format: '',
+          indent: 0,
+          version: 1,
+          direction: 'ltr',
           children: [
             {
-              text: 'This is a call to action',
-            },
-          ],
-          type: 'h4',
-        },
-        {
-          children: [
-            {
-              text: 'This is a custom layout building block ',
-            },
-            {
-              type: 'link',
-              linkType: 'custom',
-              url: '/admin',
+              type: 'paragraph',
+              format: '',
+              indent: 0,
+              version: 1,
+              direction: 'ltr',
               children: [
                 {
-                  text: 'configured in the admin dashboard',
+                  mode: 'normal',
+                  type: 'text',
+                  style: '',
+                  detail: 0,
+                  version: 1,
+                  format: 1,
+                  text: 'This is a call to action',
                 },
               ],
             },
-            {
-              text: '.',
-            },
           ],
         },
-      ],
+      },
       links: [
         {
           link: {
