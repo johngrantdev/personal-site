5388c70cd8e2294ce5793ba6eb981303e78a8021
John Grant <john@raur.net>
2023-11-23 01:00:58 +1100

    implement fade transitions

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -1,16 +1,13 @@
 'use client'
-
 import React, { Fragment, useCallback, useEffect, useRef, useState } from 'react'
+import { animated, useTransition } from '@react-spring/web'
 import qs from 'qs'
 
 import { Post } from '../../../payload/payload-types'
 import type { ArchiveBlockProps } from '../../_blocks/ArchiveBlock/types'
 import { Card } from '../Card'
-import { Gutter } from '../Gutter'
 import { PageRange } from '../PageRange'
 import { Pagination } from '../Pagination'
-
-// import classes from './index.module.scss'
 
 type Result = {
   totalDocs: number
@@ -106,7 +103,7 @@ export const CollectionArchive: React.FC<Props> = props => {
                     in:
                       typeof catsFromProps === 'string'
                         ? [catsFromProps]
-                        : catsFromProps.map(cat => cat.id).join(','),
+                        : catsFromProps.map(category => category.id).join(','),
                   },
                 }
               : {}),
@@ -151,50 +148,45 @@ export const CollectionArchive: React.FC<Props> = props => {
     }
   }, [page, catsFromProps, relationTo, onResultChange, sort, limit, populateBy])
 
+  const [currentResult, setCurrentResult] = useState<Result>()
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentResult(results)
+    }, 300)
+  }, [results])
+
   return (
-    // add CollectionArchive with className props
     <div className={[className].filter(Boolean).join(' ')}>
-      {/* add scrollRef styles */}
-      <div className="absolute left-0 top-0" ref={scrollRef} />
-      {!isLoading && error && <Gutter>{error}</Gutter>}
-      <Fragment>
-        <Gutter>
-          {/* add grid styles */}
-          <div className=" grid grid-cols-3 w-full gap-3">
-            {results.docs?.map((result, index) => {
-              return (
-                // add column props
-                // className="col-end-4"
-                <div key={index}>
-                  <Card relationTo={relationTo} doc={result} showCategories />
-                </div>
-              )
-            })}
-          </div>
-          {results.totalPages > 1 && (
-            // add pagnation styles
-            <Pagination
-              className="mt-12"
-              page={results.page}
-              totalPages={results.totalPages}
-              onClick={setPage}
-            />
-          )}
-        </Gutter>
-        {showPageRange !== false && (
-          <Gutter>
-            {/* add pageRange styles */}
-            <div className="py-6">
-              <PageRange
-                totalDocs={results.totalDocs}
-                currentPage={results.page}
-                collection={relationTo}
-                limit={limit}
-              />
-            </div>
-          </Gutter>
-        )}
-      </Fragment>
+      <div className="absolute left-0 top-[-24]" ref={scrollRef} />
+      {!isLoading && error && <>{error}</>}
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 w-full gap-3">
+        {currentResult &&
+          currentResult.docs?.map((result, index) => {
+            return (
+              <div key={index}>
+                <Card index={index} relationTo={relationTo} doc={result} showCategories />
+              </div>
+            )
+          })}
+      </div>
+      {currentResult && currentResult.totalPages > 1 && (
+        <Pagination
+          className="mt-12"
+          page={results.page}
+          totalPages={results.totalPages}
+          onClick={setPage}
+        />
+      )}
+      {showPageRange !== false && (
+        <div className="py-6">
+          <PageRange
+            totalDocs={results.totalDocs}
+            currentPage={results.page}
+            collection={relationTo}
+            limit={limit}
+          />
+        </div>
+      )}
     </div>
   )
 }
