949a24ce0127f33dad5ef25a08894e49e16305e6
John Grant <john@raur.net>
2023-11-23 00:49:21 +1100

    verticalPadding and Gutter to Padding Component simplified block padding

--- src/app/(pages)/not-found.tsx
+++ src/app/(pages)/not-found.tsx
@@ -1,15 +1,12 @@
 import { Button } from '../_components/Button'
-import { Gutter } from '../_components/Gutter'
-import { VerticalPadding } from '../_components/VerticalPadding'
+import { Padding } from '../_components/Padding'
 
 export default function NotFound() {
   return (
-    <Gutter>
-      <VerticalPadding top="none" bottom="large">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p>This page could not be found.</p>
-        <Button href="/" label="Go Home" appearance="primary" />
-      </VerticalPadding>
-    </Gutter>
+    <Padding top={false}>
+      <h1 style={{ marginBottom: 0 }}>404</h1>
+      <p>This page could not be found.</p>
+      <Button href="/" label="Go Home" appearance="primary" />
+    </Padding>
   )
 }
