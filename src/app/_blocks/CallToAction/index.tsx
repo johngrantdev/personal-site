eba1669b3be2a7bd8ee095b441183557cbb9d815
John Grant <john@raur.net>
2023-11-23 00:53:21 +1100

    Removed Gutter, as Padding handled in Blocks

--- src/app/_blocks/CallToAction/index.tsx
+++ src/app/_blocks/CallToAction/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
 import { Page } from '../../../payload/payload-types'
-import { Gutter } from '../../_components/Gutter'
 import { CMSLink } from '../../_components/Link'
 import RichText from '../../_components/RichText'
-import { VerticalPadding } from '../../_components/VerticalPadding'
 
 // import classes from './index.module.scss'
 
@@ -16,20 +14,15 @@ export const CallToActionBlock: React.FC<
   }
 > = ({ links, richText, invertBackground }) => {
   return (
-    <Gutter>
-      {/* need to add dark theme */}
-      <div className="px-6 py-12 flex gap-14 items-center dark:bg-zinc-900 bg-zinc-300 drop-shadow-xl">
-        <div className="flex-grow">
-          {/* add richText Styling */}
-          <RichText content={richText} />
-        </div>
-        {/* todo implement the sass logic for margin-bottom */}
-        <div className="flex flex-col justify-center h-full flex-shrink-0 ">
-          {(links || []).map(({ link }, i) => {
-            return <CMSLink key={i} {...link} invert={invertBackground} />
-          })}
-        </div>
+    <div className="px-6 py-12 flex gap-14 items-center dark:bg-zinc-900 bg-zinc-600 text-zinc-100 rounded-lg drop-shadow-xl">
+      <div className="flex-grow">
+        <RichText content={richText} />
       </div>
-    </Gutter>
+      <div className="flex flex-col justify-center h-full flex-shrink-0 ">
+        {(links || []).map(({ link }, i) => {
+          return <CMSLink key={i} {...link} invert={invertBackground} />
+        })}
+      </div>
+    </div>
   )
 }
