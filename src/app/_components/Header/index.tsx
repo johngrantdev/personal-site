4c9a631b923c482856d2f75e7d77d493afd20383
John Grant <john@raur.net>
2023-11-23 01:02:04 +1100

    removed gutter, added padding component moved to Title client component

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -2,13 +2,14 @@
   /* eslint-disable @next/next/no-img-element */
 }
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 
 import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
-import { Gutter } from '../Gutter'
+import { Padding } from '../Padding'
 import { HeaderNav } from './Nav'
+import { Title } from './Title'
 
 export async function Header() {
   let header: Header | null = null
@@ -23,27 +24,12 @@ export async function Header() {
   }
 
   return (
-    <>
-      <header className="py-6 px-0">
-        {/* add wrap styles */}
-        <Gutter className="flex justify-between flex-wrap gap-x-3 gap-y-6">
-          <Link href="/">
-            {/* Cannot use the `<picture>` element here with `srcSet`
-              This is because the theme is able to be overridden by the user
-              And so `@media (prefers-color-scheme: dark)` will not work
-              Instead, we just use CSS to invert the color via `filter: invert(1)` based on `[data-theme="dark"]`
-            */}
-            {/* invert needs to be implemented with tailwind */}
-            <div className="h-full text-center text-4xl">john grant</div>
-            {/* <img
-              className="w-40 dark:invert"
-              alt="Payload Logo"
-              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
-            /> */}
-          </Link>
-          <HeaderNav header={header} />
-        </Gutter>
-      </header>
-    </>
+    <header>
+      {/* add wrap styles */}
+      <Padding className="flex justify-between flex-wrap gap-x-3" bottom={false}>
+        <Title />
+        <HeaderNav header={header} />
+      </Padding>
+    </header>
   )
 }
