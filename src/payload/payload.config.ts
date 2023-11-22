f5c347a59f5762e2302e38f8e89a50afe5807dde
John Grant <john@raur.net>
2023-11-23 10:13:43 +1100

    updated lexical configuration removed beforeDashboard component added keyword category added computeBlurhash plugin

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -4,24 +4,26 @@ import nestedDocs from '@payloadcms/plugin-nested-docs'
 import redirects from '@payloadcms/plugin-redirects'
 import seo from '@payloadcms/plugin-seo'
 import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { BlocksFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import dotenv from 'dotenv'
 import path from 'path'
 import { buildConfig } from 'payload/config'
+import computeBlurhash from 'payload-blurhash-plugin'
 
-import { Posts } from './collections/Posts'
+import { Code } from './blocks/Code'
 import Categories from './collections/Categories'
+import Keywords from './collections/Keywords'
 import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
+import { Posts } from './collections/Posts'
 import Users from './collections/Users'
-import BeforeDashboard from './components/BeforeDashboard'
-import BeforeLogin from './components/BeforeLogin'
+import { SeedButton } from './components/BeforeDashboard/SeedButton'
 import { seed } from './endpoints/seed'
 import { Footer } from './globals/Footer'
 import { Header } from './globals/Header'
 
 const generateTitle: GenerateTitle = () => {
-  return 'My Website'
+  return process.env.SITE_TITLE
 }
 
 dotenv.config({
@@ -33,12 +35,8 @@ export default buildConfig({
     user: Users.slug,
     bundler: webpackBundler(),
     components: {
-      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: [BeforeLogin],
-      // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
-      beforeDashboard: [BeforeDashboard],
+      // Seed Button
+      beforeDashboard: [SeedButton],
     },
     webpack: config => ({
       ...config,
@@ -55,14 +53,21 @@ export default buildConfig({
       },
     }),
   },
-  editor: lexicalEditor({}),
+  editor: lexicalEditor({
+    features: ({ defaultFeatures }) => [
+      ...defaultFeatures,
+      BlocksFeature({
+        blocks: [Code],
+      }),
+    ],
+  }),
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI,
     },
   }),
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [Pages, Posts, Media, Categories, Users],
+  collections: [Pages, Posts, Media, Categories, Keywords, Users],
   globals: [Header, Footer],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
@@ -87,12 +92,13 @@ export default buildConfig({
       collections: ['pages', 'posts'],
     }),
     nestedDocs({
-      collections: ['categories'],
+      collections: ['categories', 'keywords'],
     }),
     seo({
       collections: ['pages', 'posts'],
       generateTitle,
       uploadsCollection: 'media',
     }),
+    computeBlurhash(),
   ],
 })
