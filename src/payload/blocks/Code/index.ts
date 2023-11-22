00deebdd65029a3633883cde826604565483068c
John Grant <john@raur.net>
2023-11-23 10:14:28 +1100

    initial

---
+++ src/payload/blocks/Code/index.ts
@@ -0,0 +1,38 @@
+import type { Block } from 'payload/types'
+
+export const Code: Block = {
+  slug: 'code',
+  labels: {
+    singular: 'Code',
+    plural: 'Code',
+  },
+  fields: [
+    {
+      name: 'language',
+      required: true,
+      type: 'select',
+      options: [
+        {
+          label: 'Go',
+          value: 'go',
+        },
+        {
+          label: 'Javascript',
+          value: 'javascript',
+        },
+        {
+          label: 'Typescript',
+          value: 'typescript',
+        },
+      ],
+    },
+    {
+      name: 'code',
+      type: 'code',
+      required: true,
+      admin: {
+        language: 'javascript',
+      },
+    },
+  ],
+}
