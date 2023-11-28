56c148f1b4bdc145cd57a35037b1e77476eb1fdb
John Grant <john@raur.net>
2023-11-28 23:46:27 +1100

    fixed to positively check for object this ensures the next condition is valid ts

--- src/app/_components/Media/index.tsx
+++ src/app/_components/Media/index.tsx
@@ -7,7 +7,7 @@ import { Video } from './Video'
 export const Media: React.FC<Props> = props => {
   const { className, resource, htmlElement = 'div' } = props
 
-  const isVideo = typeof resource !== 'string' && resource?.mimeType?.includes('video')
+  const isVideo = typeof resource === 'object' && resource?.mimeType?.includes('video')
   const Tag = (htmlElement as any) || Fragment
 
   return (
