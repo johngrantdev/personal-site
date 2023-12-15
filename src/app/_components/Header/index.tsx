82dd8c191693fb0a55a393ac78732ae13f0dd4b6
John Grant <john@raur.net>
2023-12-15 13:32:15 +1100

    updated props for global siteSettings removed fetch added ternery logic when passing siteSettings props, avoiding errors when siteSettings is not available

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -5,30 +5,22 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
 
-import { Header } from '../../../payload/payload-types'
-import { fetchHeader } from '../../_api/fetchGlobals'
+import { Site } from '../../../payload/payload-types'
 import { Padding } from '../Padding'
 import { HeaderNav } from './Nav'
 import { Title } from './Title'
 
-export async function Header() {
-  let header: Header | null = null
+type HeaderProps = {
+  siteSettings: Site
+}
 
-  try {
-    header = await fetchHeader()
-  } catch (error) {
-    // When deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // So swallow the error here and simply render the header without nav items if one occurs
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
-  }
-
+export async function Header({ siteSettings }: HeaderProps) {
   return (
     <header>
       {/* add wrap styles */}
       <Padding className="flex justify-between flex-wrap gap-x-3" bottom={false}>
-        <Title siteName={process.env.SITE_NAME} />
-        <HeaderNav header={header} />
+        <Title siteTitle={siteSettings ? siteSettings.siteTitle : 'Title'} />
+        <HeaderNav siteSettings={siteSettings} />
       </Padding>
     </header>
   )
