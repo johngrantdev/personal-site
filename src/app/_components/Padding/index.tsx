d7beb020f0d015bdf44a6ae20557da637954493a
jayelg <john@raur.net>
2024-02-13 00:01:41 +1100

    added full height property updated styling

--- src/app/_components/Padding/index.tsx
+++ src/app/_components/Padding/index.tsx
@@ -5,21 +5,30 @@ type Props = {
   bottom?: boolean
   left?: boolean
   right?: boolean
+  fullHeight?: boolean
   className?: string
   children: React.ReactNode
   ref?: Ref<HTMLDivElement>
 }
 
 export const Padding: React.FC<Props> = forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const { top = true, bottom = true, left = true, right = true, children, className } = props
+  const {
+    top = true,
+    bottom = true,
+    left = true,
+    right = true,
+    fullHeight = false,
+    children,
+    className,
+  } = props
 
   return (
     <div
       ref={ref}
-      className={`max-w-full mx-auto ${top ? 'pt-6' : 'pt-0'} ${
-        bottom ? 'pb-6' : 'pb-0'
-      } ${className} ${left ? 'pl-6 md:pl-12 xl:pl-24' : 'pl-0'} ${
-        right ? 'pr-6 md:pr-12 xl:pr-24' : 'pr-0'
+      className={`${className} mx-auto gap-x-10 gap-y-10 lg:gap-x-24  ${top ? 'pt-24' : 'pt-0'} ${
+        bottom ? 'pb-12' : 'pb-0'
+      } ${left ? 'pl-6 md:pl-12 xl:pl-24' : 'pl-0'} ${right ? 'pr-6 md:pr-12 xl:pr-24' : 'pr-0'} ${
+        fullHeight && 'min-h-screen overflow-hidden'
       }`}
     >
       {children}
