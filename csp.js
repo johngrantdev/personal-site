8533307224b5bccf7188cadb83fa53e6128b5c97
jayelg <john@raur.net>
2024-02-22 11:30:42 +1100

    updates to rules to allow embed vimeo

--- csp.js
+++ csp.js
@@ -1,12 +1,23 @@
 const policies = {
   'default-src': ["'self'"],
-  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com'],
+  'script-src': [
+    "'self'",
+    "'unsafe-inline'",
+    "'unsafe-eval'",
+    'https://maps.googleapis.com',
+    'https://player.vimeo.com',
+  ],
   'child-src': ["'self'"],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
   'img-src': ["'self'", 'https://raw.githubusercontent.com'],
   'font-src': ["'self'"],
-  'frame-src': ["'self'"],
-  'connect-src': ["'self'", 'https://maps.googleapis.com'],
+  'frame-src': ["'self'", 'https://player.vimeo.com'],
+  'connect-src': [
+    "'self'",
+    'https://maps.googleapis.com',
+    'https://vimeo.com',
+    'https://player.vimeo.com',
+  ],
 }
 
 module.exports = Object.entries(policies)
