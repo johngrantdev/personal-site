9535c0ac5172d3a213aa0a8a7e7ffc96bed5f7d9
jayelg <john@raur.net>
2024-02-22 11:42:48 +1100

    move breakpoints

--- src/app/_components/Hero/PageHero/index.tsx
+++ src/app/_components/Hero/PageHero/index.tsx
@@ -16,11 +16,11 @@ export const PageHero: React.FC<PageHeroProps> = props => {
     <div className={className}>
       {media && <Media className="pb-4" />}
       <RichText className="py-0 mt-0" content={description} />
-      <div className="w-full justify-center mt-5 flex lg:flex-col items-center lg:items-start">
+      <div className="w-full justify-center mt-5 flex xl:flex-col items-center xl:items-start">
         {Array.isArray(links) &&
           links.length > 0 &&
           links.map((link, i) => (
-            <CMSLink className=" mx-2 lg:mx-0 my-1 lg:my-2" key={i} {...link.link} />
+            <CMSLink className=" mx-2 xl:mx-0 my-1 xl:my-2" key={i} {...link.link} />
           ))}
       </div>
     </div>
