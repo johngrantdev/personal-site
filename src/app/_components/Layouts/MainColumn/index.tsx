2dd1cb37241d8d57f783075541a2462547bf8bd3
jayelg <john@raur.net>
2024-03-12 09:05:49 +1100

    add option in layout for post archive

--- src/app/_components/Layouts/MainColumn/index.tsx
+++ src/app/_components/Layouts/MainColumn/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
 import { MainColumn as MainColumnType } from '../../../../payload/payload-types'
-import RichText from '../../RichText'
+import { CollectionArchive } from '../../CollectionArchive'
+import RichText from '../../RichText/static'
 
 export type MainColumnProps = MainColumnType & {
   hasTOC?: boolean
 }
 
 export const MainColumn: React.FC<MainColumnProps> = props => {
-  const { style, column1, column2, hasTOC = false } = props
+  const { style, postArchive, column1, column2, hasTOC = false } = props
 
   switch (style) {
+    case 'postArchive':
+      return <CollectionArchive className="w-full" {...postArchive} />
     case 'singleLayout':
       return <RichText className="w-full" content={column1} hasTOC />
     case 'twoColumns':
