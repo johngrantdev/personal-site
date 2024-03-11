f480dac908e5833d47f5d8ba68fa204984e13afe
jayelg <john@raur.net>
2024-03-12 09:03:12 +1100

    client component no longer required

--- src/app/_components/Button/index.tsx
+++ src/app/_components/Button/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { ElementType } from 'react'
 import Link from 'next/link'
 
--- src/app/_components/Layout/index.tsx
+++ src/app/_components/Layout/index.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { forwardRef, ReactElement, ReactNode, Ref } from 'react'
 
 import { Padding } from '../Padding'
